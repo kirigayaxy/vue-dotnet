161b491fef0b7bd247fab49964012b82cbe9e2c6
Pongrawee Sripetpong <pongrawee.sripe@mail.kmutt.ac.th>
2025-07-01 18:04:45 +0700

    fix vue api url

--- vue3/projectvue/src/components/Login.vue
+++ vue3/projectvue/src/components/Login.vue
@@ -32,7 +32,7 @@ export default {
         Password: this.password,
 
       };
-      const API_URL = process.env.VUE_APP_API_URL || "http://localhost:5096";
+      const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5096";
       console.log("Sending username for login:", this.username);
       axios
         .post(`${API_URL}/Auth/login`, userData)
